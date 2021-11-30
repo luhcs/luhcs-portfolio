a2db03016dcdbda0b265786d61f5e5b161c27085
luhcs <lucas_fgomes@CAMP-CMP-01.localdomain>
2021-11-30 18:16:01 -0300

    Adding Repo

---
+++ repo/Devs/pages/index.js
@@ -0,0 +1,60 @@
+import { Container, Box, Heading, Image, useColorModeValue, Button, Link } from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import NextLink from 'next/link'
+const Page = () => {
+	return (
+		<Container>
+			<Box 
+			borderRadius="lg" 
+			bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
+			p={3} 
+			mb={6} 
+			align="center">
+				Olá, seja bem vindo ao meu portfolio! :)
+			</Box>
+
+			<Box display={{ md: 'flex' }}>
+				<Box flexGrow={1}>
+					<Heading as="h2" variant="page-title">
+						Lucas F. Gomes
+					</Heading>
+					<p>Aprendiz de programação (Estudante de Analise e Desenvolvimento de Sistemas / Economia)</p>
+				</Box>
+				<Box flexShrink={0}
+					mt={{ base: 4, md: 0 }}
+					ml={{ md: 6 }}
+					align="center"
+				>
+					<Image
+						borderColor="whiteAlpha.800"
+						borderWidth={2}
+						borderStyle="solid"
+						maxWidth="100px"
+						display="inline-block"
+						borderRadius="full"
+						src="/images/lucas.jpg"
+						alt="Imagem de Perfil"
+					/>
+				</Box>
+			</Box>
+
+			<Section delay={0.1}>
+				<Heading as="h3" variant="section-title">
+					Sobre
+				</Heading>
+				<Paragraph>Lucas é um iniciante e apaixonado pelo mundo da programação, e que pretende viver dela. Atualmente ele trabalha como Assistente de Cadastro, mas não deixa a programação de lado, especialmente em suas macros no excel.</Paragraph>
+			<Box align="center" my={4}>
+				<NextLink href="/trabalhos">
+					<Button rightIcon={<ChevronRightIcon/>} colorScheme="gray">
+						Meu portfolio
+					</Button>
+				</NextLink>
+			</Box>
+			</Section>
+		</Container>
+	)
+}
+
+export default Page
