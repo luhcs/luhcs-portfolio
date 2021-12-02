745fb239eb925a21ad8a8aa4628c202062a8d438
luhcs <lucas_fgomes@CAMP-CMP-01.localdomain>
2021-12-02 16:20:32 -0300

    Adicionado tela de erro.

---
+++ repo/Devs/pages/404.js
@@ -0,0 +1,27 @@
+import NextLink from 'next/link'
+import {
+    Box,
+    Heading,
+    Text,
+    Container,
+    Divider,
+    Button
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+    return (
+        <Container>
+            <Heading as="h1">Não Encontrado</Heading>
+            <Text>A página que você procura não foi encontrada.</Text>
+            <Divider my={6} />
+
+            <Box my={6} align="center">
+                <NextLink href="/">
+                    <Button colorScheme="gray">Voltar</Button>
+                </NextLink>
+            </Box>
+        </Container>  
+    )
+}
+
+export default NotFound
